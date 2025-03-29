59bee00a3e5e8218b141e286d99734d099aba689
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-03-29 21:21:41 +0800

    fixed storage error for non json parse

--- src/js/Storage.js
+++ src/js/Storage.js
@@ -1,8 +1,15 @@
 const storage = (type, key, value) => {
   const store = type === "local" ? localStorage : sessionStorage;
-  if (value === undefined) return JSON.parse(store.getItem(key));
+  if (value === undefined) {
+    try {
+      return JSON.parse(store.getItem(key));
+    } catch (e) {
+      return store.getItem(key);
+    }
+  }
   if (value === null) return store.removeItem(key);
   store.setItem(key, JSON.stringify(value));
 };
+
 const $local = (key, value) => storage("local", key, value);
 const $session = (key, value) => storage("session", key, value);
