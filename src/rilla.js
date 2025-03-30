c9bef04b423f22abb1263f20839c86e25456b623
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-03-30 17:35:42 +0800

    added spacing

--- src/rilla.js
+++ src/rilla.js
@@ -111,11 +111,13 @@ Element.prototype.html = function (content) {
   this.innerHTML = content;
   return this;
 };
+
 Element.prototype.text = function (content) {
   if (content === undefined) return this.textContent;
   this.textContent = content;
   return this;
 };
+
 Element.prototype.val = function (value) {
   if (value === undefined) return this.value;
   this.value = value;
