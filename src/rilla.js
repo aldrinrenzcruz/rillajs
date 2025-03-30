934c03c4376f9fba9dbe5a030e06f519710cecfa
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-03-30 19:56:40 +0800

    added wrap unwrap

--- src/rilla.js
+++ src/rilla.js
@@ -106,6 +106,86 @@ Element.prototype.$paint = function (htmlString) {
   return this;
 };
 
+Element.prototype.wrap = function (content) {
+  const temp = document.createElement("div");
+  temp.innerHTML = content;
+  const wrapper = temp.firstElementChild;
+  if (!wrapper) {
+    console.error("$wrap: invalid argument");
+    return this;
+  }
+  const parent = this.parentNode;
+  if (parent) {
+    parent.insertBefore(wrapper, this);
+    wrapper.appendChild(this);
+  }
+  return this;
+};
+
+NodeList.prototype.wrap = HTMLCollection.prototype.wrap = function (content) {
+  const elements = Array.from(this);
+  elements.forEach(element => {
+    if (element instanceof Element) {
+      element.wrap(content);
+    }
+  });
+  return this;
+};
+
+Element.prototype.unwrap = function () {
+  const parent = this.parentNode;
+  if (!parent || parent === document.documentElement) {
+    return this;
+  }
+  const grandparent = parent.parentNode;
+  if (grandparent) {
+    grandparent.insertBefore(this, parent);
+    let onlyWhitespaceLeft = true;
+    for (let i = 0; i < parent.childNodes.length; i++) {
+      const node = parent.childNodes[i];
+      if (node.nodeType !== 3 || node.nodeValue.trim() !== '') {
+        onlyWhitespaceLeft = false;
+        break;
+      }
+    }
+    if (parent.childNodes.length === 0 || onlyWhitespaceLeft) {
+      grandparent.removeChild(parent);
+    }
+  }
+  return this;
+};
+
+NodeList.prototype.unwrap = function () {
+  const elements = Array.from(this);
+  const parents = new Set();
+  elements.forEach(element => {
+    if (element instanceof Element) {
+      const parent = element.parentNode;
+      if (parent && parent !== document.documentElement) {
+        parents.add(parent);
+        const grandparent = parent.parentNode;
+        if (grandparent) {
+          grandparent.insertBefore(element, parent);
+        }
+      }
+    }
+  });
+  parents.forEach(parent => {
+    let onlyWhitespaceLeft = true;
+    for (let i = 0; i < parent.childNodes.length; i++) {
+      const node = parent.childNodes[i];
+      if (node.nodeType !== 3 || node.nodeValue.trim() !== '') {
+        onlyWhitespaceLeft = false;
+        break;
+      }
+    }
+    if (parent.childNodes.length === 0 || onlyWhitespaceLeft) {
+      parent.parentNode?.removeChild(parent);
+    }
+  });
+  return this;
+};
+
 Element.prototype.html = function (content) {
   if (content === undefined) return this.innerHTML;
   this.innerHTML = content;
