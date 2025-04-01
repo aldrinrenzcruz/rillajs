520bfe60afe3541b2785df2db2b88fb69242b57d
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-04-01 11:44:28 +0800

    Add .outer() method

--- dist/rilla.js
+++ dist/rilla.js
@@ -143,6 +143,34 @@ HTMLCollection.prototype.html = function (content) {
   return this;
 };
 
+Element.prototype.outer = function (content) {
+  if (content === undefined) {
+    return this.outerHTML;
+  }
+  this.outerHTML = content;
+  return this;
+};
+
+NodeList.prototype.outer = function (content) {
+  if (content === undefined) {
+    return Array.from(this).map(el => el.outerHTML);
+  }
+  for (let i = 0; i < this.length; i++) {
+    this[i].outerHTML = content;
+  }
+  return this;
+};
+
+HTMLCollection.prototype.outer = function (content) {
+  if (content === undefined) {
+    return Array.from(this).map(el => el.outerHTML);
+  }
+  for (let i = 0; i < this.length; i++) {
+    this[i].outerHTML = content;
+  }
+  return this;
+};
+
 Element.prototype.parent = function () {
   return this.parentNode;
 };
---
+++ src/DOM Manipulation/outer.js
@@ -0,0 +1,27 @@
+Element.prototype.outer = function (content) {
+  if (content === undefined) {
+    return this.outerHTML;
+  }
+  this.outerHTML = content;
+  return this;
+};
+
+NodeList.prototype.outer = function (content) {
+  if (content === undefined) {
+    return Array.from(this).map(el => el.outerHTML);
+  }
+  for (let i = 0; i < this.length; i++) {
+    this[i].outerHTML = content;
+  }
+  return this;
+};
+
+HTMLCollection.prototype.outer = function (content) {
+  if (content === undefined) {
+    return Array.from(this).map(el => el.outerHTML);
+  }
+  for (let i = 0; i < this.length; i++) {
+    this[i].outerHTML = content;
+  }
+  return this;
+};
