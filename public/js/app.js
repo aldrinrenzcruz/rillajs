f4e7263c39f808052807b15639517a2cebef0174
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-04-01 09:20:17 +0800

    Create examples json and fetch

--- public/js/app.js
+++ public/js/app.js
@@ -16,50 +16,56 @@ function initializeSidebarSmoothScroll() {
   });
 }
 
-const examples = ["dom-selection", "installation", "dom-manipulation", "element-references", "attributes-classes", "event-handling", "document-window", "visibility", "storage", "draggable", "global-usage"];
-
 function renderLandingCodeBlocks() {
-  Promise.all(
-    examples.map(async example => {
-      const url = `./public/docs/code-snippets/landing/${example}.txt`;
-      return fetch(url)
-        .then(response => {
-          if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-          return response.text();
+  fetch('./public/docs/code-snippets/landing/landing.json')
+    .then(response => response.json())
+    .then(data => {
+      const examples = data.examples;
+      return Promise.all(
+        examples.map(async example => {
+          const url = `./public/docs/code-snippets/landing/${example}.txt`;
+          return fetch(url)
+            .then(response => {
+              if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+              return response.text();
+            })
+            .then(data => {
+              const lines = data.split("\n");
+              const lang = lines[0].replace(/^#\s*/, "");
+              const modifiedContent = lines.slice(2).join("\n");
+              return { example, modifiedContent, lang };
+            })
+            .catch(err => {
+              console.error(`Error fetching ${url}:`, err);
+              return { example, data: null, lang };
+            });
         })
-        .then(data => {
-          const lines = data.split("\n");
-          const lang = lines[0].replace(/^#\s*/, "");
-          const modifiedContent = lines.slice(2).join("\n");
-          return { example, modifiedContent, lang };
-        })
-        .catch(err => {
-          console.error(`Error fetching ${url}:`, err);
-          return { example, data: null, lang };
+      ).then(results => {
+        results.forEach(({ example, modifiedContent, lang }) => {
+          $(`.code-block[data-src=${example}]`)
+            .$append(`<pre></pre>`)
+            .$("pre")
+            .$append(`<code class="language-${lang}"></code>`)
+            .$("code")
+            .text(modifiedContent)
+            .$this((el) => { hljs.highlightElement(el) })
+            .parent()
+            .wrap("<div class='relative'></div>")
+            .parent()
+            .$append("<button class='absolute top-2 right-2 px-2 py-1 bg-gray-800 text-gray-400 text-xs rounded hover:bg-gray-700 transition hover:cursor-pointer'></button>")
+            .$("button")
+            .text(lang.trim() || "copy")
+            .$this(btn =>
+              btn.on('click', () => {
+                navigator.clipboard.writeText(modifiedContent);
+                btn.text("copied!");
+                setTimeout(() => btn.text(lang || "copy"), 1500);
+              })
+            );
         });
+      });
     })
-  ).then(results => {
-    results.forEach(({ example, modifiedContent, lang }) => {
-      $(`.code-block[data-src=${example}]`)
-        .$append(`<pre></pre>`)
-        .$("pre")
-        .$append(`<code class="language-${lang}"></code>`)
-        .$("code")
-        .text(modifiedContent)
-        .$this((el) => { hljs.highlightElement(el) })
-        .parent()
-        .wrap("<div class='relative'></div>")
-        .parent()
-        .$append("<button class='absolute top-2 right-2 px-2 py-1 bg-gray-800 text-gray-400 text-xs rounded hover:bg-gray-700 transition hover:cursor-pointer'></button>")
-        .$("button")
-        .text(lang.trim() || "copy")
-        .$this(btn =>
-          btn.on('click', () => {
-            navigator.clipboard.writeText(modifiedContent);
-            btn.text("copied!");
-            setTimeout(() => btn.text(lang || "copy"), 1500);
-          })
-        )
+    .catch(err => {
+      console.error("Error loading .json:", err);
     });
-  });
 }
