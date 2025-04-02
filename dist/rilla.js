bfeab2b84390582c94d571e272c2f857325eeaef
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-04-02 12:15:30 +0800

    Add $data method

--- dist/rilla.js
+++ dist/rilla.js
@@ -622,6 +622,60 @@ NodeList.prototype.$class = function (classNames) {
   return this;
 };
 
+Element.prototype.$data = function (key, value) {
+  // Case 1: No parameters - get all data attributes
+  if (arguments.length === 0) {
+    const dataAttributes = {};
+    Array.from(this.attributes)
+      .filter(attr => attr.name.startsWith('data-'))
+      .forEach(attr => {
+        const keyName = attr.name.slice(5);
+        dataAttributes[keyName] = attr.value;
+      });
+    return dataAttributes;
+  }
+  // Case 2: Empty string parameter - remove all data attributes
+  if (key === "" && arguments.length === 1) {
+    const dataAttrs = Array.from(this.attributes)
+      .filter(attr => attr.name.startsWith('data-'))
+      .map(attr => attr.name);
+    dataAttrs.forEach(attrName => {
+      this.removeAttribute(attrName);
+    });
+    return this;
+  }
+  // Convert key to data-attribute format
+  const dataKey = `data-${key}`;
+  // Case 3: Get specific data attribute value
+  if (arguments.length === 1) {
+    return this.getAttribute(dataKey);
+  }
+  // Case 4: Set specific data attribute value
+  if (value === "") {
+    this.removeAttribute(dataKey);
+  } else {
+    this.setAttribute(dataKey, value);
+  }
+  return this;
+};
+
+NodeList.prototype.$data = function (key, value) {
+  if (arguments.length === 0) {
+    return Array.from(this).map(element => {
+      if (element instanceof Element) {
+        return element.$data();
+      }
+      return null;
+    });
+  }
+  Array.from(this).forEach(element => {
+    if (element instanceof Element) {
+      Element.prototype.$data.apply(element, arguments);
+    }
+  });
+  return this;
+};
+
 Element.prototype.addClass = function (classNames) {
   if (typeof classNames !== "string") {
     console.error("addClass: input must be a string");
