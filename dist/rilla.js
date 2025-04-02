23904676466ac399ff17a418b894588fdd4b2618
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-04-02 14:04:59 +0800

    Update $paint to accept blank string to empty

--- dist/rilla.js
+++ dist/rilla.js
@@ -91,10 +91,6 @@ window.$create = function (tagName) {
 
 Element.prototype.$paint = function (htmlString) {
   this.innerHTML = "";
-  if (!htmlString) {
-    console.error("$paint: invalid parameter");
-    return this;
-  }
   const template = document.createElement("template");
   template.innerHTML = htmlString.trim();
   const fragment = template.content;
@@ -103,10 +99,6 @@ Element.prototype.$paint = function (htmlString) {
 };
 
 NodeList.prototype.$paint = function (htmlString) {
-  if (!htmlString) {
-    console.error("$paint: invalid parameter");
-    return this;
-  }
   this.forEach(element => {
     if (element instanceof Element) {
       element.$paint(htmlString);
