efaca615170f61c7c56d7c855ea82013a9dd1047
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-06-12 23:54:18 +0800

    Remove paint method

--- dist/rilla.js
+++ dist/rilla.js
@@ -222,24 +222,6 @@ NodeList.prototype.outer = function (content) {
   return this;
 };
 
-Element.prototype.$paint = function (htmlString) {
-  this.innerHTML = "";
-  const template = document.createElement("template");
-  template.innerHTML = htmlString.trim();
-  const fragment = template.content;
-  this.append(fragment);
-  return this;
-};
-
-NodeList.prototype.$paint = function (htmlString) {
-  this.forEach(element => {
-    if (element instanceof Element) {
-      element.$paint(htmlString);
-    }
-  });
-  return this;
-};
-
 Element.prototype.$prepend = function (htmlString) {
   if (!htmlString) {
     console.warn("$prepend: invalid parameter");
