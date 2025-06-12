7190822a6d4d81c40fa15244fd588ad05a206fa2
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2025-06-13 00:15:23 +0800

    Delete create method

--- dist/rilla.js
+++ dist/rilla.js
@@ -178,14 +178,6 @@ NodeList.prototype.$before = function (htmlString) {
   return this;
 };
 
-window.$create = function (tagName) {
-  if (!tagName || typeof tagName !== "string") {
-    console.warn("$create: invalid tag name");
-    return null;
-  }
-  return document.createElement(tagName);
-};
-
 Element.prototype.$inner = function (content) {
   if (content === undefined) {
     return this.innerHTML;
--- src/DOM Manipulation/create.js
+++
@@ -1,7 +0,0 @@
-window.$create = function (tagName) {
-  if (!tagName || typeof tagName !== "string") {
-    console.warn("$create: invalid tag name");
-    return null;
-  }
-  return document.createElement(tagName);
-};
